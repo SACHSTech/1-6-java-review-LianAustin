6e2c1eaa5c4762e9505cb5c432fc5a51a90d1e6e
LianAustin <austin.lian25@ycdsbk12.ca>
2024-09-12 22:20:10 -0400

    Completed problem Review5

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,37 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/**
+ * A program that asks the user for the yearly amount, compound interest rate, and target amount
+ * and outputs the number of years it will take to reach the target amount
+ *
+ * @author: Austin L
+ */
+public class Review5{
+    public static void main(String[] args) throws IOException{
+
+        // Initialize variables
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double dblCurrentAmount = 0;
+        int intYears = 0;
+
+        // Ask the user for the yearly amount, compound interest rate, and target amount
+        System.out.print("Enter the yearly invested amount: ");
+        double dblYearlyAmount = Double.parseDouble(br.readLine());
+        System.out.print("Enter the compound interest rate: ");
+        double dblInterestRate = Double.parseDouble(br.readLine());
+        System.out.print("Enter the target amount: ");
+        double dblTargetAmount = Double.parseDouble(br.readLine());
+
+        // Calculate the number of years it will take to reach the target amount
+        while (dblCurrentAmount < dblTargetAmount) {
+            dblCurrentAmount += dblYearlyAmount;
+            dblCurrentAmount += dblCurrentAmount * (dblInterestRate / 100);
+            intYears++;
+        }
+
+        // Output the number of years it will take to reach the target amount
+        System.out.println("The target amount will be earned in " + intYears + " years.");
+    }
+}
