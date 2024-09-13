6a0b8f909ae6d6ba5972342d85c1e80fd9bcb6de
LianAustin <austin.lian25@ycdsbk12.ca>
2024-09-12 23:07:27 -0400

    Ensured adherence to style guide (made BigDecimal type prefix bdl, fixed commenting and some whitespace issues)

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -5,31 +5,37 @@ import java.math.*;
 import java.text.*;
 
 /**
- * A program that asks the user for the price of items and outputs the subtotal, tax, and total
+ * A program that asks the user for the bdlPrice of items and outputs the bdlSubTotal, bdlTax, and bdlTotal
  * of the items
  *
  * @author: Austin L
  */
 public class Review4 {
     public static void main(String[] args) throws Exception {
+
+        // Initialize variables
+        int intItems;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         DecimalFormat df = new DecimalFormat("0.00");
 
+        // Input price of items
         System.out.print("How many items do you want to buy? ");
-        int intItems = Integer.parseInt(br.readLine());
-        BigDecimal subtotal = BigDecimal.ZERO;
+        intItems = Integer.parseInt(br.readLine());
+        BigDecimal bdlSubTotal = BigDecimal.ZERO;
 
+        // Calculate the SubTotal, Tax, and Total of the items
         for (int i = 1; i <= intItems; i++) {
-            System.out.print("Enter the price for item " + i + ": ");
-            BigDecimal price = new BigDecimal(br.readLine());
-            subtotal = subtotal.add(price);
+            System.out.print("Enter the bdlPrice for item " + i + ": ");
+            BigDecimal bdlPrice = new BigDecimal(br.readLine());
+            bdlSubTotal = bdlSubTotal.add(bdlPrice);
         }
 
-        BigDecimal tax = subtotal.multiply(new BigDecimal("0.13")).setScale(2, RoundingMode.HALF_UP);
-        BigDecimal total = subtotal.add(tax);
+        BigDecimal bdlTax = bdlSubTotal.multiply(new BigDecimal("0.13")).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal bdlTotal = bdlSubTotal.add(bdlTax);
 
-        System.out.println("Subtotal: $" + df.format(subtotal));
-        System.out.println("Tax: $" + df.format(tax));
-        System.out.println("Total: $" + df.format(total));
+         // Output the SubTotal, Tax, and Total of the items
+        System.out.println("SubbdlTotal: $" + df.format(bdlSubTotal));
+        System.out.println("Tax: $" + df.format(bdlTax));
+        System.out.println("Total: $" + df.format(bdlTotal));
     }
 }
