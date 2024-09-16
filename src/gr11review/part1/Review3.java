2615e0db80877173c59650aed2e96614bfbe4ccc
LianAustin <austin.lian25@ycdsbk12.ca>
2024-09-16 10:03:10 -0400

    Updated Review1 to adhere to style guide (fixed javadocs comment)

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -2,11 +2,18 @@ package gr11review.part1;
 
 import java.io.*;
 
+/**
+ * A program that outputs odd numbers from 20 to 100 and numbers from 29 to 2 in decreasing order
+ *
+ * @author: Austin L
+ */
 public class Review3 {
+
     /**
-     * A program that outputs odd numbers from 20 to 100 and numbers from 29 to 2 in decreasing order
+     * Main method to execute the program logic.
      *
-     * @author: Austin L
+     * @param args Command-line arguments (not used).
+     * @throws IOException If there is an error during input.
      */
     public static void main(String[] args) {
         for (int i = 21; i < 100; i += 2) {
